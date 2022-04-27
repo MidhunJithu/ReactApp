a872cbfffb0b8c5179e7dd1c023311a404313c2e
Midhunjithu <midhun.m@techversantinfo.com>
2022-04-27 20:57:20 +0530

    Api call to fetch posts added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React  ,{ Suspense} from 'react';
+import{ Layout} from './pages/Navbar';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import { Comments } from './pages/Comments';
+import Contacts from './pages/Contacts';
+import ContactInfo from './pages/ContactInfo';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { TopBar } from './utilities';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            color: props.color,
+            style : 'mustang',
+            make : 2021
+        };
+    }
+    changeColor = () => {this.setState(
+        {color: this.state.color === 'red'? 'blue':'red'});
+    }
+    render(){
+        return(
+            <>
+            <TopBar>
+                <h2 className='centre'>Welcome to Digital Library</h2>
+            </TopBar>
+            <BrowserRouter>
+                <Routes>
+                    
+                    <Route path='/' element={<Layout/>}>
+                        <Route path ='/Home' element={<Home/>}></Route>
+                        <Route path ='/Comments' element={<Comments/>}></Route>
+                        <Route path='Blogs' element={<Blogs/>}></Route>
+                        <Route path='Contacts' element={<Contacts/>}>
+                            <Route index element={<h3>Select a Contact</h3>}></Route>
+                            <Route path=':ContactId' element={<ContactInfo/>}></Route>
+                        </Route>
+                        <Route path='*' element={<main><p>No INFO FOUND</p></main>}></Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>  
+            </>
+            
+        )
+    }
 }
-
 export default App;
